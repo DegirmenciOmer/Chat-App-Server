93b3d509382bece8b2a94fd37df07ad461737ff7
Omer Degirmenci <rumidegirmenci0@gmail.com>
2021-09-09 18:47:33 +0200

    commit

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 import express from 'express'
 export const router = express.Router()
-import PORT from './server.js'
+import { PORT } from './server.js'
 
 router.get('/', (req, res) => {
   res.send(`Server is up and running on ${PORT}`)
